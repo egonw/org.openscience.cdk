167989e8c8a01eac6da4edbe343f266dee64aaaa
Arvid Berg <goglepox@users.sourceforge.net>
2010-06-22 14:09:45 +0200

    Enables AlterBondStereoModule to cycle between different stereo modes
    UP->UP_INVERTED->NONE->UP...
    DOWN->DOWN_INVERTED->NONE->DOWN...

--- plugins/org.openscience.cdk.controlbasic/src/org/openscience/cdk/controller/AlterBondStereoModule.java
+++ plugins/org.openscience.cdk.controlbasic/src/org/openscience/cdk/controller/AlterBondStereoModule.java
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2008  Gilleain Torrance <gilleain.torrance@gmail.com>
  * Copyright (C) 2008  Stefan Kuhn (undo redo)
- * Copyright (C) 2009  Arvid Berg <goglepox@users.sf.net>
+ * Copyright (C) 2009-2010  Arvid Berg <goglepox@users.sf.net>
  *
  * Contact: cdk-devel@lists.sourceforge.net
  *
@@ -35,6 +35,7 @@ import org.openscience.cdk.controller.edit.SetStereo;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.interfaces.IChemObject;
+import org.openscience.cdk.interfaces.IBond.Stereo;
 import org.openscience.cdk.renderer.selection.AbstractSelection;
 
 
@@ -46,6 +47,9 @@ import org.openscience.cdk.renderer.selection.AbstractSelection;
  * @cdk.module controlbasic
  */
 public class AlterBondStereoModule extends ControllerModuleAdapter {
+
+    Stereo[] switchOrderUp = new Stereo[] {Stereo.UP,Stereo.UP_INVERTED,Stereo.NONE};
+    Stereo[] switchOrderDown = new Stereo[] {Stereo.DOWN,Stereo.DOWN_INVERTED,Stereo.NONE};
 
     private IBond.Stereo desiredDirection;
 
@@ -73,13 +77,29 @@ public class AlterBondStereoModule extends ControllerModuleAdapter {
 		} else if (singleSelection instanceof IAtom) {
 		    chemModelRelay.execute( AppendAtom.appendAtom( "C", atom, desiredDirection ) );
 		} else if (singleSelection instanceof IBond) {
-			super.chemModelRelay.execute( SetStereo.setStereo( bond, desiredDirection ) );
+		    Stereo bondStereo = bond.getStereo();
+		    switch(desiredDirection) {
+		        case UP: setStereo(bond, findNextStereoMode(bondStereo, switchOrderUp));break;
+		        case DOWN: setStereo(bond, findNextStereoMode(bondStereo, switchOrderDown));break;
+		    }
 		} else {
 			// by default, change the bond
-		    super.chemModelRelay.execute( SetStereo.setStereo( bond, desiredDirection ) );
+		    setStereo( bond, desiredDirection );
 		}
 
 	}
+
+	private void setStereo(IBond bond, Stereo mode) {
+	    super.chemModelRelay.execute(SetStereo.setStereo(bond, mode));
+	}
+
+  private Stereo findNextStereoMode(Stereo current,Stereo[] switchOrder) {
+      for(int i=0;i<switchOrder.length;i++) {
+          if(current == switchOrder[i])
+              return switchOrder[i+1==switchOrder.length?0:i+1];
+      }
+      return switchOrder[0];
+  }
 
 	public String getDrawModeString() {
 		if (desiredDirection == IBond.Stereo.UP) {
